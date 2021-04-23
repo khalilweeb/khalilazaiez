1b042ed89faebe581b049089d98439f553a27812
Your Name <you@example.com>
2021-04-23 03:11:44 +0100

    project

---
+++ app.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('#navbarMenu');
+const menuLinks = document.querySelector('.navbar_menu');
+
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+
+});
+
+
+
+
+ 
+  
+  
+ 
